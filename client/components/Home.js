3984e75d90c998645c1029afaf4454ca8e02b002
awu556 <austinw556@gmail.com>
2020-11-18 22:01:02 -0500

    simple homepage

---
+++ client/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1>
+        You've decided to enter the world of professional mixed martial arts
+      </h1>
+      <h2>But you'll need a good nickname before you enter.</h2>
+      <button>Enter the cage</button>
+    </div>
+  );
+};
+
+export default Home;
--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 
-router.use("/stockData", require("./stockData"));
 router.use("/users", require("./users"));
 
 module.exports = router;
