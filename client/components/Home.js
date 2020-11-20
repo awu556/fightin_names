0d09d3789dede52e1daecfbcf1ed928ff6e71205
awu556 <austinw556@gmail.com>
2020-11-19 21:06:12 -0500

    added inputs for first/last name

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import {HomeDiv, RandomizeForm} from './styledComponents/styledHome'
 
 const Home = (props) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  const handleChangeFirst = (evt) => {
+    setFirstName(evt.target.value)
+  }
+
+  const handleChangeSecond = (evt) => {
+    setLastName(evt.target.value)
+  }
+
+  console.log(firstName, lastName)
   return (
-    <div>
+    <HomeDiv>
       <h1>
-        You've decided to enter the world of professional mixed martial arts
+        You've decided to enter the world of professional mixed martial arts...
       </h1>
       <h2>But you'll need a good nickname before you enter.</h2>
-      <button>Enter the cage</button>
-    </div>
+
+      <RandomizeForm>
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="fname" value={firstName} onChange={handleChangeFirst}/>
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lname" value={lastName} onChange={handleChangeSecond}/>
+        <button>Randomize me a nickname!</button>
+      </RandomizeForm>
+      
+    </HomeDiv>
   );
 };
 
---
+++ client/components/styledComponents/styledHome.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const HomeDiv = styled.div`
+    text-align: center;
+`
+
+export const RandomizeForm = styled.form`
+    display: inline-block;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: left;
+`
